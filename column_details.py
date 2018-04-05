fe4a0a0c3141d7b32902c2260c5dbaf4e25ee033
Balita1124 <ricomroatfauchard08@gmail.com>
2018-04-05 12:10:05 +0300

    Rectification

--- column_details.py
+++ column_details.py
@@ -14,19 +14,18 @@ worksheet = workbook.add_worksheet()
 bold = workbook.add_format({'bold': True})
 
 worksheet.write('A1', u"Modèle", bold)
-worksheet.write('B1', 'Codification', bold)
-worksheet.write('C1', u"Désignation", bold)
-worksheet.write('D1', 'Type', bold)
-worksheet.write('E1', 'Taille', bold)
-worksheet.write('F1', 'Observation', bold)
+worksheet.write('B1', u"Description Modèle", bold)
+worksheet.write('C1', 'Codification', bold)
+worksheet.write('D1', u"Désignation", bold)
+worksheet.write('E1', 'Type', bold)
+worksheet.write('F1', 'Taille', bold)
+worksheet.write('G1', 'Obligatoire', bold)
 
 
 
 conn = psycopg2.connect("host='localhost' dbname='socolait_v1.4.11' user='odoo10' password='odoo10' ")
-print conn.encoding
 conn.set_client_encoding('latin1')
 cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
-print conn.encoding
 
 in_workbook = xlrd.open_workbook("model.xlsx")
 in_worksheet = in_workbook.sheet_by_index(0)
@@ -47,26 +46,20 @@ for model in models:
     for row in cur:
         table = row['table_name']
         cur_row = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
-##        cur_row.execute(u"""SELECT description FROM pg_catalog.pg_description WHERE objsubid = 
-##            (
-##                SELECT ordinal_position FROM  information_schema.columns WHERE table_name='%s' AND column_name='%s'
-##            ) 
-##            and objoid = 
-##            (
-##                SELECT oid FROM pg_class WHERE relname = 'account_account' AND relnamespace = 
-##                            (
-##                                SELECT oid FROM pg_catalog.pg_namespace WHERE nspname = 'public'
-##                            )
-##                    )""" % (model,row['column_name']) )
         dot_model = model.replace('_','.')
         
         query = """
-                    select field_description, size, required from ir_model_fields where model_id = (select id from ir_model where model = '%s' limit 1) and name = '%s'
+                    select imf.field_description, imf.size, imf.required, im.name
+                    from ir_model_fields imf
+                    left join ir_model im on im.id = imf.model_id
+                    where im.model = '%s' and imf.name = '%s'
                 """ % (dot_model,row['column_name'])
         cur_row.execute(query)
         description  = ""
+        model_description  = ""
         for r in cur_row:
             description = r[0] if r else ""
+            model_description = r[3] if r else ""
             if description:
                 cur_trans = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
                 helping = description.replace("'","''")
@@ -78,20 +71,26 @@ for model in models:
                 cur_trans.execute(sql)
                 for rr in cur_trans:
                     description = rr[0] if rr else description
+        if model_description:
+                cur_trans = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
+                helping = model_description.replace("'","''")
+                sql = """
+                    SELECT value FROM ir_translation
+                    WHERE lang='fr_FR' AND type in ('model') AND 
+                    src='%s'
+                """ % helping
+                cur_trans.execute(sql)
+                for rr in cur_trans:
+                    model_description = rr[0] if rr else model_description
       
         worksheet.write(iterator, 0, row['table_name'])
-        worksheet.write(iterator, 1, row['column_name'])
-        worksheet.write(iterator, 2, description.decode('latin1'))
-        worksheet.write(iterator, 3, row['data_type'])
-        worksheet.write(iterator, 4, row['character_maximum_length'] if row['character_maximum_length'] else row['numeric_precision'])
-        worksheet.write(iterator, 5, row['is_nullable'])
+        worksheet.write(iterator, 1, model_description.decode('latin1'))
+        worksheet.write(iterator, 2, row['column_name'])
+        worksheet.write(iterator, 3, description.decode('latin1'))
+        worksheet.write(iterator, 4, row['data_type'])
+        worksheet.write(iterator, 5, row['character_maximum_length'] if row['character_maximum_length'] else row['numeric_precision'])
+        worksheet.write(iterator, 6, row['is_nullable'])
         iterator += 1
 workbook.close()
-    
-    #print "schema: {schema}, table: {table}, column: {col}, type: {type}, description: {description}".format(
-        #schema = row['table_schema'], table = row['table_name'],
-        #col = row['column_name'], type = row['data_type'], description = description)
-    #print "column: {col}, type: {type}, description: {description}".format(
-        #col = row['column_name'], type = row['data_type'], description = description)
     
     
